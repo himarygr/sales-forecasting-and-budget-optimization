b6da559ecfc419c4af3804f08e52d3cdd5031021
Maria Lagosha <86712204+himarygr@users.noreply.github.com>
2023-08-10 13:55:54 +0300

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,20 +1,18 @@
 import pandas as pd
 
+
 def load_data(file_path):
-    """Load the CSV data file."""
+    """Load the dataset from a CSV file."""
     return pd.read_csv(file_path)
 
-def preprocess_train_data(train_data):
-    """Preprocess the training data."""
+
+def preprocess_data(train_data, test_data, sample_submission):
+    """Preprocess the dataset."""
     train_data['date'] = pd.to_datetime(train_data['date'])
-    return train_data
-
-def preprocess_test_data(test_data):
-    """Preprocess the test data."""
+    preprocessed_train = train_data
     test_data['date'] = pd.to_datetime(test_data['date'])
-    return test_data
-
-def preprocess_sample_submission(sample_submission):
-    """Preprocess the sample submission data."""
+    preprocessed_test = test_data
     sample_submission['sales'] = 0  # Initialize sales as 0 for the sample submission
-    return sample_submission
+    preprocessed_sample_submission = sample_submission
+    
+    return preprocessed_train, preprocessed_test, preprocessed_sample_submission
